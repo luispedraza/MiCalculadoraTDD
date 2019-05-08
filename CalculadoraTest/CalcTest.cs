039b7f7d7a577db277ae1db30c029631fb9a82b0
Luis <luispedraza@gmail.com>
2019-05-08 11:35:06 +0200

    inicializado el framework de pruebas

---
+++ CalculadoraTest/CalcTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTest
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        [Test]
+        public void prueba1()
+        {
+            Assert.Pass();
+        }
+    }
+}
