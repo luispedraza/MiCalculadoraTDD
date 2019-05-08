a2c4d38c81d9070366e736dfd305a99457a6a27c
Luis <luispedraza@gmail.com>
2019-05-08 11:26:51 +0200

    commit inicial; proyecto creado

---
+++ MiCalculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
